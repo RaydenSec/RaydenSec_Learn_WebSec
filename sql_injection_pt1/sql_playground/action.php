f065853db1c77ac2786005ca74f3bd9ebca5cf1a
Rayden <raydenwang883@gmail.com>
2022-11-17 04:40:40 +1100

    Added first part of the SQL Injection guide and code

---
+++ sql_injection_pt1/sql_playground/action.php
@@ -0,0 +1,33 @@
+<?php include 'connect.php' ?>
+<?php
+    global $connection; 
+
+    // validate result
+    function validate($result) {
+        if (!$result) {
+            die("Query Failed"); 
+        } 
+    }
+
+    // SQL test
+    if (isset($_POST['sql_submit'])) {
+        $username = $_POST['sql_username']; 
+        $password = $_POST['sql_password'];  
+
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";  
+
+        $result = mysqli_query($connection, $query);  
+        validate($result);  
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+            <pre>
+            <?php
+            print_r($row); 
+            ?>
+            </pre>
+            <?php
+            echo "<br>";   
+        }
+    }
+?>   
